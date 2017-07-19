f2c3704b33fc45b9c6d5ef31ca5a4f34f6a4866c
mloszek <miloszloszek@gmail.com>
2017-07-19 12:35:30 +0200

    next commit; warmup challenge

--- src/main/java/ThirdyDaysOfCode/Day5/Solution.java
+++ src/main/java/ThirdyDaysOfCode/Day5/Solution.java
@@ -1,13 +1,19 @@
 package ThirdyDaysOfCode.Day5;
 
+import java.util.Scanner;
+
 /**
  * Day 5 of 30 days challenge
  */
 public class Solution {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
 
-
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
 
     }
 }
---
+++ src/main/java/ThirdyDaysOfCode/Day6/Solution.java
@@ -0,0 +1,14 @@
+package ThirdyDaysOfCode.Day6;
+
+/**
+ * Day 5 of 30 days challenge
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
