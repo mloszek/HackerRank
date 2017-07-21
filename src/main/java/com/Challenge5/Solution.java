88e04733899ace7fca6b758c9c3a7498ea1d073a
mloszek <miloszloszek@gmail.com>
2017-07-21 20:30:22 +0200

    👶

---
+++ src/main/java/com/Challenge5/Solution.java
@@ -0,0 +1,43 @@
+package com.Challenge5;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Warmup: Birthday Cake Candles
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = birthdayCakeCandles(n, ar);
+        System.out.println(result);
+    }
+
+    static int birthdayCakeCandles(int n, int[] ar) {
+
+        List<Integer> top = new ArrayList<>();
+
+        int temp = 0;
+
+        for (int i = 0; i < ar.length; i++) {
+            if (i == 0)
+            temp = ar[i];
+            else if (ar[i] > temp)
+                temp = ar[i];
+        }
+
+        for (int i = 0; i < ar.length; i++) {
+            if (ar[i] == temp)
+                top.add(ar[i]);
+        }
+
+        return top.size();
+    }
+}
