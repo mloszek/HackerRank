0962feaf17905158497d1abd1bce1807abf30b27
mloszek <miloszloszek@gmail.com>
2017-07-25 15:59:05 +0200

    next commit; implementation challenges;

--- src/main/java/ThirdyDaysOfCode/Day12/Student.java
+++ src/main/java/ThirdyDaysOfCode/Day12/Student.java
@@ -1,22 +1,38 @@
 package ThirdyDaysOfCode.Day12;
 
-class Student extends Person{
+class Student extends Person {
 
     private int[] testScores;
 
-    /*
-    *   Class Constructor
-    *
-    *   @param firstName - A string denoting the Person's first name.
-    *   @param lastName - A string denoting the Person's last name.
-    *   @param id - An integer denoting the Person's ID number.
-    *   @param scores - An array of integers denoting the Person's test scores.
-    */
-    // Write your constructor here
+    public Student(String firstName, String lastName, int identification, int[] testScores) {
+        super(firstName, lastName, identification);
+        this.testScores = testScores;
+    }
 
-    /*
-    *   Method Name: calculate
-    *   @return A character denoting the grade.
-    */
-    // Write your method here
+    public String calculate() {
+
+        int result = 0;
+
+        for (int i = 0; i < testScores.length; i++) {
+            result += testScores[i];
+        }
+
+        result = result / testScores.length;
+
+        if (result >= 90 && result <= 100)
+            return "O";
+        if (result >= 80 && result < 90)
+            return "E";
+        if (result >= 70 && result < 80)
+            return "A";
+        if (result >= 55 && result < 70)
+            return "P";
+        if (result >= 40 && result < 55)
+            return "D";
+        if (result < 40)
+            return "T";
+
+        return "shit";
+
+    }
 }
