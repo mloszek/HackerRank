d19927f79ba9094e1d76ffc342de7319e81b0e09
mloszek <miloszloszek@gmail.com>
2017-07-28 14:02:48 +0200

    next commit; implementation challenges; 30 days of code up to day 15

---
+++ src/main/java/ThirdyDaysOfCode/Day15/Node.java
@@ -0,0 +1,13 @@
+package ThirdyDaysOfCode.Day15;
+
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
--- src/main/java/ThirdyDaysOfCode/Day15/Solution.java
+++ src/main/java/ThirdyDaysOfCode/Day15/Solution.java
@@ -1,7 +1,44 @@
 package ThirdyDaysOfCode.Day15;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Day 15 of 30 days challenge
  */
 public class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while (N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head, ele);
+        }
+        display(head);
+        sc.close();
+    }
+
+    public static void display(Node head) {
+        Node start = head;
+        while (start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static Node insert(Node head, int data) {
+
+        if (head == null)
+            head = new Node(data);
+        else {
+            head.next = new Node(data);
+
+        }
+
+
+        return head;
+    }
 }
