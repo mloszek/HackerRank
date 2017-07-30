debf9af8d0eaba713bc01732461036d047daa00c
mloszek <miloszloszek@gmail.com>
2017-07-30 13:44:24 +0200

    next commit; implementation challenges; 30 days of code up to day 17

---
+++ src/main/java/Algorithms/Implementation/Implementation3/Kangaroo.java
@@ -0,0 +1,28 @@
+package Algorithms.Implementation.Implementation3;
+
+public class Kangaroo {
+
+    private int position;
+    private int velocity;
+
+    public Kangaroo(int position, int velocity) {
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public int getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+}
--- src/main/java/Algorithms/Implementation/Implementation3/Solution.java
+++ src/main/java/Algorithms/Implementation/Implementation3/Solution.java
@@ -1,8 +1,81 @@
 package Algorithms.Implementation.Implementation3;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
 /**
  * Implementation: Kangaroo
  */
 public class Solution {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        String result = kangaroo(x1, v1, x2, v2);
+
+        System.out.println(result);
+
+    }
+
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+
+        List<Kangaroo> kangarooList = new ArrayList<>();
+        kangarooList.add(new Kangaroo(x1, v1));
+        kangarooList.add(new Kangaroo(x2, v2));
+
+
+        if (isAlreadyWin(kangarooList)) {
+            return "YES";
+        }
+
+        if (isAlreadyLoose(kangarooList)){
+            return "NO";
+        }
+
+        kangarooList.sort(Comparator.comparingInt(Kangaroo::getVelocity));
+
+        int firstKangPos = kangarooList.get(1).getPosition();
+        int secKangPos = kangarooList.get(0).getPosition();
+
+        while (true) {
+
+            firstKangPos += kangarooList.get(1).getVelocity();
+
+            secKangPos += kangarooList.get(0).getVelocity();
+
+            if (firstKangPos == secKangPos)
+                return "YES";
+
+            if (firstKangPos > secKangPos)
+                return "NO";
+        }
+
+    }
+
+    public static boolean isAlreadyWin(List<Kangaroo> list) {
+
+        List<Kangaroo> spec = list;
+
+
+        if ((list.get(0).getPosition() == list.get(1).getPosition()) && (list.get(0).getVelocity() == list.get(1).getVelocity()))
+            return true;
+
+        return false;
+    }
+
+    public static boolean isAlreadyLoose(List<Kangaroo> list) {
+
+        for (int i = 0; i < 1; i++) {
+            if ((list.get(1 - i).getPosition() > list.get(0 + i).getPosition()) && (list.get(1 - i).getVelocity() > list.get(0 + i).getVelocity()))
+                return true;
+        }
+
+        return false;
+    }
 }
