2db20e17f13d4519dba9dc1d76de05b57f45d9f5
mloszek <miloszloszek@gmail.com>
2017-08-01 10:32:16 +0200

    next commit; sorting 1'st challenge yet to do; 30 days of code up to day 19

--- src/main/java/Algorithms/Sorting/Sorting1/Solution.java
+++ src/main/java/Algorithms/Sorting/Sorting1/Solution.java
@@ -1,7 +1,6 @@
 package Algorithms.Sorting.Sorting1;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * Sorting: Big Sorting
@@ -14,25 +13,39 @@ public class Solution {
         int n = in.nextInt();
         String temp = null;
 
-        List<String> list = new ArrayList<>();
+        String[] tab = new String[n];
 
         for(int i=0; i < n; i++){
-            list.add(in.next());
+            tab[i] = in.next();
         }
 
-        Comparator<String> comparator = Comparator.comparing(Integer::valueOf);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (isBigger(tab[j], tab[j + 1])) {
+                    temp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = temp;
+                }
+            }
+        }
 
-        Collections.sort(list, comparator);
-
-        System.out.println(String.valueOf(list));
+        for (String s: tab) {
+            System.out.println(s);
+        }
 
     }
 
     public static boolean isBigger(String a, String b){
 
-        for (int i = 0; i < a.length(); i++) {
-            if (Integer.parseInt(String.valueOf(a.charAt(i))) > Integer.parseInt(String.valueOf(b.charAt(i)))){
-                return true;
+        if (a.length() > b.length())
+            return true;
+        else if (a.length() < b.length())
+            return false;
+        else {
+            for (int i = 0; i < a.length(); i++) {
+                if (Integer.parseInt(String.valueOf(a.charAt(i))) > Integer.parseInt(String.valueOf(b.charAt(i)))) {
+                    return true;
+                }
             }
         }
 
