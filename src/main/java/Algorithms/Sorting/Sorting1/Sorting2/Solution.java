23f12335a408a125d4fed9aa4ef29739d35e08f2
mloszek <miloszloszek@gmail.com>
2017-08-13 12:56:49 +0200

    next commit; warmup done; implementation up to problem 4; sorting first problem done; 30 days of code up to day 28;

---
+++ src/main/java/Algorithms/Sorting/Sorting1/Sorting2/Solution.java
@@ -0,0 +1,12 @@
+package Algorithms.Sorting.Sorting1.Sorting2;
+
+/**
+ * Sorting: Intro to Tutorial Challenges
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+
+
+    }
+}
--- src/main/java/ThirdyDaysOfCode/Day28/Solution.java
+++ src/main/java/ThirdyDaysOfCode/Day28/Solution.java
@@ -1,10 +1,29 @@
 package ThirdyDaysOfCode.Day28;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 /**
- * Day 27 of 30 days challenge
+ * Day 28 of 30 days challenge
  */
 public class Solution {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int rows = scanner.nextInt();
+        List<String> names = new ArrayList<>();
+        for (int i = 0; i < rows; i++) {
+            String name = scanner.next();
+            String email = scanner.next();
+            if (email.contains("@gmail.com"))
+                names.add(name);
+
+        }
+
+        names.stream()
+                .sorted()
+                .forEach(System.out::println);
     }
 }
