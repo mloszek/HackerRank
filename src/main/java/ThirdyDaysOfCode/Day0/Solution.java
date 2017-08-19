883f7ad3d7392c28dc76de464bb86aa76c34946a
mloszek <miloszloszek@gmail.com>
2017-08-19 08:40:08 +0200

    next commit; warmup done; implementation up to problem 6; sorting up to problem 2; 30 days of code up to day 28;

---
+++ src/main/java/ThirdyDaysOfCode/Day0/Solution.java
@@ -0,0 +1,16 @@
+package ThirdyDaysOfCode.Day0;
+
+import java.util.Scanner;
+
+/**
+ * Day 0 of 30 days challenge
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+        scan.close();
+        System.out.println("Hello, World.\n" + inputString);
+    }
+}
