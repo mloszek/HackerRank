4c62db8f2a1b3007b8229de8ac179502ee82e84c
mloszek <miloszloszek@gmail.com>
2017-09-02 17:32:27 +0200

    next commit; 30 days of code completed warmup completed implementation up to problem 8 + problem 12, 22 sorting up to problem 2 dynamic programming problem 7 strings up to problem 4 (unfinished) + problem 5

--- src/main/java/Algorithms/Strings/Strings3/Solution.java
+++ src/main/java/Algorithms/Strings/Strings3/Solution.java
@@ -3,6 +3,7 @@ package Algorithms.Strings.Strings3;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,20 +23,72 @@ public class Solution {
         System.out.println(result);
     }
 
-    static int longestPossibleString(List<Character> letters){
+     static int longestPossibleString(List<Character> letters) {
 
         List<Character> possibleLetters = lettersUsed(letters);
 
-
-
-
+        if (possibleLetters.size() == 1 || letters.size() < 2)
+            return 0;
+        else if (possibleLetters.size() == 2)
+            return lettersCleaner(letters, possibleLetters.get(0), possibleLetters.get(1));
+        else 
+            return lengthTester(possibleLetters, letters);
+        
     }
 
-    static List<Character> lettersUsed(List<Character> letters){
+     static int lengthTester(List<Character> possibleLetters, List<Character> allLetters) {
+
+        int result = 0;
+
+        for (int i = 0; i < possibleLetters.size() - 1; i++) {
+            for (int j = i + 1; j < possibleLetters.size(); j++) {
+                int temp = lettersCleaner(allLetters, possibleLetters.get(i), possibleLetters.get(j));
+                if (temp > result)
+                    result = temp;
+            }
+        }
+
+        return result;
+    }
+
+     static int lettersCleaner(List<Character> allLetters, char a, char b) {
+
+         List<Character> cleanedList = new ArrayList<>();
+
+         for (int i = 0; i < allLetters.size(); i++) {
+             if (allLetters.get(i) == a || allLetters.get(i) == b)
+                 cleanedList.add(allLetters.get(i));
+         }
+
+        return doesAlternate(cleanedList) ? cleanedList.size() : 0 ;
+    }
+
+     static boolean doesAlternate(List<Character> cleanedLettersList) {
+
+        char a = ' ';
+        char b = ' ';
+
+        for (int i = 0; i < cleanedLettersList.size(); i++) {
+            if (i == 0)
+                a = cleanedLettersList.get(i);
+            else if (i == 1 && cleanedLettersList.get(i) == a)
+                return false;
+            else if (i == 1 && cleanedLettersList.get(i) != a)
+                b = cleanedLettersList.get(i);
+            else if (i % 2 == 0 && cleanedLettersList.get(i) != a)
+                return false;
+            else if (i % 2 != 0 && cleanedLettersList.get(i) != b)
+                return false;
+        }
+
+        return true;
+    }
+
+     static List<Character> lettersUsed(List<Character> letters) {
 
         List<Character> result = new ArrayList<>();
 
-        for (char c: letters) {
+        for (char c : letters) {
             if (!result.contains(c))
                 result.add(c);
         }
@@ -43,7 +96,7 @@ public class Solution {
         return result;
     }
 
-    static List<Character> getAllLetters(String s){
+     static List<Character> getAllLetters(String s) {
 
         return Stream.of(s.split(""))
                 .map(letter -> letter.charAt(0))
---
+++ src/main/java/Algorithms/Strings/Strings4/Solution.java
@@ -0,0 +1,12 @@
+package Algorithms.Strings.Strings4;
+
+/**
+ * Strings: Caesar Cipher
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+
+
+    }
+}
