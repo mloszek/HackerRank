6f754aaab8731174d0ba2636179b6e36990c0c78
mloszek <miloszloszek@gmail.com>
2017-09-02 15:00:31 +0200

    next commit; 30 days of code completed warmup completed implementation up to problem 8 + problem 12, 22 sorting up to problem 2 dynamic programming problem 7 strings up to problem 3 (unfinished) + problem 5

--- src/main/java/Algorithms/Strings/Strings3/Solution.java
+++ src/main/java/Algorithms/Strings/Strings3/Solution.java
@@ -1,12 +1,52 @@
 package Algorithms.Strings.Strings3;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Strings: Two Characters
  */
 public class Solution {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        String s = scanner.next();
+
+        List<Character> allLetters = getAllLetters(s);
+        int result = longestPossibleString(allLetters);
+
+        System.out.println(result);
+    }
+
+    static int longestPossibleString(List<Character> letters){
+
+        List<Character> possibleLetters = lettersUsed(letters);
 
 
+
+
+    }
+
+    static List<Character> lettersUsed(List<Character> letters){
+
+        List<Character> result = new ArrayList<>();
+
+        for (char c: letters) {
+            if (!result.contains(c))
+                result.add(c);
+        }
+
+        return result;
+    }
+
+    static List<Character> getAllLetters(String s){
+
+        return Stream.of(s.split(""))
+                .map(letter -> letter.charAt(0))
+                .collect(Collectors.toList());
     }
 }
