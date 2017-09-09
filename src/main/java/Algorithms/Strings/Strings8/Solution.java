5cfb36bd7413a161d87ca53bdea30839675023c1
mloszek <miloszloszek@gmail.com>
2017-09-09 23:13:15 +0200

    unfinished string 8

--- src/main/java/Algorithms/Strings/Strings8/Solution.java
+++ src/main/java/Algorithms/Strings/Strings8/Solution.java
@@ -5,84 +5,75 @@ import java.util.*;
 /**
  * Strings: Weighted Uniform Strings
  */
-    public class Solution {
+public class Solution {
 
-        static Map<Character, Integer> letterMap = createLetterMap();
-        static Scanner in = new Scanner(System.in);
-        static List<Integer> queries = new ArrayList<>();
+    static Map<Character, Integer> letterMap = createLetterMap();
+    static Scanner in = new Scanner(System.in);
+    static List<Integer> queries = new ArrayList<>();
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            String s = in.next();
-            int n = in.nextInt();
+        String s = in.next();
+        int n = in.nextInt();
 
-            for(int a0 = 0; a0 < n; a0++) {
-                queries.add(in.nextInt());
-            }
-
-            checkWeights(s);
+        for (int a0 = 0; a0 < n; a0++) {
+            queries.add(in.nextInt());
         }
 
-        static void checkWeights(String string) {
+        checkWeights(s);
+    }
 
-            List<String> uniformStrings = splitToUniformStrings(string);
-            List<Integer> integerValueOfUniforms = swapStringToInt(uniformStrings);
+    static void checkWeights(String string) {
 
-            for (Integer i: queries) {
-                if (integerValueOfUniforms.contains(i))
-                    System.out.println("Yes");
-                else
-                    System.out.println("No");
-            }
-        }
+        List<Integer> integerValueOfUniforms = splitToUniformStrings(string);
 
-        static List<String> splitToUniformStrings(String string) {
-
-            String[] splitted = string.split("");
-            List<String> tempList = new ArrayList<>();
-            String temp = "";
-
-            for (int i = 0; i < splitted.length; i++) {
-                if (temp == "") {
-                    temp = splitted[i];
-                    tempList.add(temp);
-                } else {
-                    if (String.valueOf(temp.charAt(temp.length() - 1)).equals(splitted[i])) {
-                        temp += splitted[i];
-                        tempList.add(temp);
-                    } else {
-                        temp = splitted[i];
-                        tempList.add(temp);
-                    }
-                }
-            }
-
-            return tempList;
-        }
-
-        static List<Integer> swapStringToInt(List<String> list) {
-
-            List<Integer> tempList = new ArrayList<>();
-
-            for (int i = 0; i < list.size(); i++) {
-                tempList.add(list.get(i).length() * letterMap.get(list.get(i).charAt(0)));
-            }
-
-            return tempList;
-        }
-
-        static Map<Character, Integer> createLetterMap() {
-
-    //        List<Character> lowerCaseAlphabet = Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-    //                'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-            char[] lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-            Map<Character, Integer> tempMap = new HashMap<>();
-
-            for (int i = 0; i < lowerCaseAlphabet.length; i++) {
-
-                tempMap.put(lowerCaseAlphabet[i], i + 1);
-            }
-
-            return tempMap;
+        for (Integer i : queries) {
+            if (integerValueOfUniforms.contains(i))
+                System.out.println("Yes");
+            else
+                System.out.println("No");
         }
     }
+
+    static List<Integer> splitToUniformStrings(String string) {
+
+        String[] splitted = string.split("");
+        List<Integer> tempList = new ArrayList<>();
+        String temp = "";
+
+        for (int i = 0; i < splitted.length; i++) {
+            if (temp == "") {
+                temp = splitted[i];
+                tempList.add(swapStringToInt(temp));
+            } else {
+                if (temp.contains(splitted[i])) {
+                    temp += splitted[i];
+                    tempList.add(swapStringToInt(temp));
+                } else {
+                    temp = splitted[i];
+                    tempList.add(swapStringToInt(temp));
+                }
+            }
+        }
+
+        return tempList;
+    }
+
+    static Integer swapStringToInt(String word) {
+
+        return word.length() * letterMap.get(word.charAt(0));
+    }
+
+    static Map<Character, Integer> createLetterMap() {
+
+        char[] lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        Map<Character, Integer> tempMap = new HashMap<>();
+
+        for (int i = 0; i < lowerCaseAlphabet.length; i++) {
+
+            tempMap.put(lowerCaseAlphabet[i], i + 1);
+        }
+
+        return tempMap;
+    }
+}
